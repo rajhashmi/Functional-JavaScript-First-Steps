4e732984be7407185baa4bff669341aab5df383a
shahid <shahidhashmiraj@gmail.com>
2023-07-24 23:24:52 +0530

    doing Exercise questions

--- app.js
+++ app.js
@@ -46,3 +46,69 @@ const thesis2 = renameThesisNoSideEffect(thesis,"church-Turing")
 
 
 
+//  =========================== Recurssion =========================
+
+// Staying out of the loops with recurssion
+
+// 1.Iteration 
+
+// Imperative looping Statefull
+
+// 2. Recussion
+
+// Functional Self-Repential Stateless
+
+// * Iteration isn't Functional use Recurssion instead
+
+
+// #  Iteration: 
+
+function sum(number){
+    let total = 0;
+    for(let i = 0; i<number.length;i++){
+        total += number[i]
+    }
+    return total
+}
+sum([0,1,2,3,4])
+
+
+//  # Recursion:
+
+// Eg. 1: ->
+function sum2(numbers){
+    debugger;
+    if(numbers.length === 1){
+        return numbers[0]
+    }else{
+        return numbers[0] + sum2(numbers.slice(1));
+    }
+}
+console.log(sum2([0,1,2,3,4]));
+
+// Eg. 2: ->
+function numbers(n){
+    console.log(n);
+    if(n<10){
+      numbers(n+1)
+    }
+}
+let number = 0;
+console.log(numbers(number));
+
+// Eg. 3: -> 
+
+function factorial(item){
+    if(item==0){
+        return 1
+    }
+    return item*factorial(item - 1)
+}
+let findFactorial = 5
+console.log(factorial(5));
+
+
+
+// =============== Itration vs Recursion Exercise ===================
+
+
